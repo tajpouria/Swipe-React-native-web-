9da8840229f73e43bf399379007c35d09daf0913
Pouria <charliswager@gmail.com>
2019-03-24 12:59:10 +0430

    to make the cards move wrap that with animation.view for setup style style={this.state.position.getLayout()} ==>

--- src/src/Deck.js
+++ src/src/Deck.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import { View, PanResponder } from "react-native";
+import { View, PanResponder, Animated } from "react-native";
 
 export default class Deck extends Component {
   constructor(props) {
     super(props);
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onPanResponderMove: (evt, gestureState) => {
-        console.log(gestureState);
+        position.setValue({ x: gestureState.dx, y: gestureState.dy });
       },
       onPanResponderRelease: () => {}
     });
-
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
   renderCards() {
     return this.props.data.map(item => {
@@ -21,9 +21,12 @@ export default class Deck extends Component {
   }
   render() {
     return (
-      <View {...this.state.panResponder.panHandlers} style={{ flex: 1 }}>
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
