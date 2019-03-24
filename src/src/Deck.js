680c4131f4707e978e40c07a4d5067dbd104c50f
Pouria <charliswager@gmail.com>
2019-03-24 21:41:05 +0430

    we define static defaultProps : {} it contain some function if this functions is n t in props it will establish instead

--- src/src/Deck.js
+++ src/src/Deck.js
@@ -6,6 +6,10 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 export default class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  };
   constructor(props) {
     super(props);
     const position = new Animated.ValueXY();
@@ -37,9 +41,10 @@ export default class Deck extends Component {
   }
 
   onSwipeComplete(direction) {
-    const { onSwipeLeft, onSwipeRight } = this.props;
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
 
-    direction = "right" ? onSwipeRight() : onSwipeLeft();
+    direction = "right" ? onSwipeRight(item) : onSwipeLeft(item);
   }
 
   resetPosition() {
